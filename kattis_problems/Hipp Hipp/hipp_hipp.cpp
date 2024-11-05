e1571b9224030d071841dd0c1e698ec2eae4ad9a
nullgar <technicianaguilar@gmail.com>
2024-11-04 21:55:37 -0800

    Second problem hipp_hipp

---
+++ kattis_problems/Hipp Hipp/hipp_hipp.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std; 
+
+int main() 
+{
+    for (int idx = 0; idx < 20; idx++) {
+        string msg = "Hipp hipp hurra!";
+        cout << msg << "\n";
+    }
+}
