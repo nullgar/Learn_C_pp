0d67d2e6aec67abddd2b009a07077b085099dac9
nullgar <technicianaguilar@gmail.com>
2024-11-05 23:16:59 -0800

    Finished storafmaeli

---
+++ kattis_problems/Storafmaeli/storafmaeli.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void checkValue(int n);
+
+int main()
+{
+    int n;
+    cin >> n;
+    void checkValue(int n);
+    
+    checkValue(n);
+
+}
+
+void checkValue(int n)
+{
+    if (n % 10 == 0) 
+    {
+        cout <<  "Jebb";
+    } 
+    else if (n % 10 != 0)
+    {
+        cout << "Neibb";
+    } 
+}
+
+
