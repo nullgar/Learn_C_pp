7d11079867023fa4a01ce187992f5f7c0ef0aa9e
nullgar <technicianaguilar@gmail.com>
2024-11-12 19:47:39 -0800

    Finished the digit swap problem

---
+++ kattis_problems/DigitSwap/digit_swap.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string input;
+
+    cin >> input;
+
+    int l = input.length() - 1;
+
+    while (l--)
+    {
+        input[l];
+    }
+    
+    // for (int i = input.length() - 1; i >= 0; i--)
+    // {
+    //     cout << input[i];
+    // }
+}
