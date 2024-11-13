886adf907e24cea739dba75a551f2ab42d83213f
nullgar <technicianaguilar@gmail.com>
2024-11-12 19:48:50 -0800

    Corrected digit swap cpp file

--- kattis_problems/DigitSwap/digit_swap.cpp
+++ kattis_problems/DigitSwap/digit_swap.cpp
@@ -7,16 +7,9 @@ int main()
     string input;
 
     cin >> input;
-
-    int l = input.length() - 1;
-
-    while (l--)
-    {
-        input[l];
-    }
     
-    // for (int i = input.length() - 1; i >= 0; i--)
-    // {
-    //     cout << input[i];
-    // }
+    for (int i = input.length() - 1; i >= 0; i--)
+    {
+        cout << input[i];
+    }
 }
