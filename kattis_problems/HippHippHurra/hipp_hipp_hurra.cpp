3260cd2ecfc37109d169e09c3e42636a923df879
nullgar <technicianaguilar@gmail.com>
2024-11-29 23:20:42 -0800

    Finished Hipp Hipp Hurra

---
+++ kattis_problems/HippHippHurra/hipp_hipp_hurra.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string name;
+    int c;
+
+    cin >> name >> c;
+
+    while (c--) {
+        cout << "Hipp hipp hurra, " << name << "!" << endl;
+    }
+}
