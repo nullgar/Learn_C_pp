6f7507fe8c1819bf9e2a6ee1c29fc721f0341787
nullgar <technicianaguilar@gmail.com>
2024-12-14 23:09:01 -0800

    Finished carrots problem

---
+++ kattis_problems/Carrots/carrots.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n,p;
+    
+    cin >> n >> p;
+    
+    while(n--) {
+        string word;
+        
+        cin >> word;
+    }
+    
+    cout << p << endl;
+}
