8b13407b290ac1d9bdc8ceae976c4b9aae99d6d2
nullgar <technicianaguilar@gmail.com>
2024-12-21 19:58:40 -0800

    Finished the "Knight Packing" problem.

---
+++ kattis_problems/KnightPacking/knightpacking.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int input;
+
+    cin >> input;
+
+    if (input % 2 != 0) {
+        cout << "first" << endl;
+    } else {
+        cout << "second" << endl;
+    }
+}
